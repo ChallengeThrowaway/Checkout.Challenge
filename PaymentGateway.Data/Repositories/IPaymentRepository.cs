64e0d1e9d50e2a3a933ff48e1abb3399571b5d79
ADeveloper <harrytennent12@hotmail.com>
2020-08-02 19:25:53 +0100

    Add basic payment repository implementation

---
+++ PaymentGateway.Data/Repositories/IPaymentRepository.cs
@@ -0,0 +1,12 @@
+﻿using PaymentGateway.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentGateway.Data.Repositories
+{
+    public interface IPaymentRepository
+    {
+        void Add(Payment payment);
+    }
+}
---
+++ PaymentGateway.Data/Repositories/PaymentRepository.cs
@@ -0,0 +1,25 @@
+﻿using PaymentGateway.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentGateway.Data.Repositories
+{
+    public class PaymentRepository : IPaymentRepository
+    {
+
+        private readonly PaymentGatewayContext _context; 
+        
+        public PaymentRepository(PaymentGatewayContext context)
+        {
+            _context = context;            
+        }
+
+        public void Add(Payment payment) 
+        {
+            _context.Add(payment);
+            _context.SaveChanges();
+        }
+
+    }
+}
