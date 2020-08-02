d4a9d6e8d6aea35e2fe9e2335a1db3107547acdf
ADeveloper <harrytennent12@hotmail.com>
2020-08-02 15:26:14 +0100

    Add basic project skeleton

---
+++ PaymentGateway.Api/Controllers/PaymentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentGateway.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult CreatePayment() 
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public ActionResult<object> GetPayment()
+        {
+            return Ok("Test");
+        }
+    }
+}
