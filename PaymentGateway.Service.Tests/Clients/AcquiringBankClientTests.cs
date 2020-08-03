98bb9f21abdb63f69a0f4aa23c5e25cb347cb1df
ADeveloper <harrytennent12@hotmail.com>
2020-08-03 21:39:48 +0100

    Add additional scenarios to cover remaining statuses

--- PaymentGateway.Service.Tests/Clients/AcquiringBankClientTests.cs
+++ PaymentGateway.Service.Tests/Clients/AcquiringBankClientTests.cs
@@ -1,5 +1,4 @@
-﻿using Castle.Core.Logging;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
 using Moq.Protected;
@@ -9,7 +8,6 @@ using PaymentGateway.Core.Enums;
 using PaymentGateway.Core.Models;
 using PaymentGateway.Service.Clients;
 using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -49,10 +47,10 @@ namespace PaymentGateway.Service.Tests.Clients
         {
             // Arrange
             PaymentRequest paymentRequest = null;
-            
+
             _httpMessageHandlerMock.Protected()
                 .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
-                .ReturnsAsync(new HttpResponseMessage { StatusCode = HttpStatusCode.OK});
+                .ReturnsAsync(new HttpResponseMessage { StatusCode = HttpStatusCode.OK });
 
             var client = new HttpClient(_httpMessageHandlerMock.Object);
 
@@ -68,7 +66,7 @@ namespace PaymentGateway.Service.Tests.Clients
             // Arrange
             var expectedBankId = new Guid("7b6bf96c-0231-41e3-9014-393ff87ed4e1");
 
-            PaymentRequest paymentRequest = new PaymentRequest 
+            PaymentRequest paymentRequest = new PaymentRequest
             {
                 Amount = 100,
                 CardholderName = "Testy McTester",
@@ -92,7 +90,8 @@ namespace PaymentGateway.Service.Tests.Clients
 
             _httpMessageHandlerMock.Protected()
                 .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
-                .ReturnsAsync(new HttpResponseMessage { 
+                .ReturnsAsync(new HttpResponseMessage
+                {
                     StatusCode = HttpStatusCode.OK,
                     Content = contentMock
                 });
@@ -107,6 +106,96 @@ namespace PaymentGateway.Service.Tests.Clients
             // Assert
             Assert.AreEqual(expectedBankId, result.BankId);
             Assert.AreEqual(PaymentStatuses.Submitted, result.PaymentStatus);
+            Assert.AreEqual(submissionDateTimeMock, result.StatusDateTime);
+        }
+
+        [Test]
+        public async Task SubmitPaymentToBank_ValidationErrorPaymentRequest_ReturnsCorrectDetails()
+        {
+            // Arrange
+            PaymentRequest paymentRequest = new PaymentRequest
+            {
+                Amount = 100,
+                CardholderName = "Testy McTester",
+                CardNumber = "1111 2222 3333 4444",
+                CurrencyIsoAlpha3 = "GBP",
+                Cvv = "666",
+                MerchantId = new Guid("aa9f4ef9-35ab-4a10-8cf5-f5535582fd01")
+            };
+
+            var submissionDateTimeMock = new DateTimeOffset(2020, 01, 01, 0, 0, 0, new TimeSpan(0, 0, 0));
+
+            AcquiringBankResponse responseMock = new AcquiringBankResponse
+            {
+                PaymentStatus = "ValidationError",
+                StatusDateTime = submissionDateTimeMock
+            };
+
+            var data = JsonSerializer.Serialize(responseMock);
+            var contentMock = new StringContent(data.ToString(), Encoding.UTF8, "application/json");
+
+            _httpMessageHandlerMock.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = contentMock
+                });
+
+            var client = new HttpClient(_httpMessageHandlerMock.Object);
+
+            var sut = new AcquiringBankClient(_acquiringBankSettingsMock.Object, client, _loggerMock.Object);
+
+            // Act 
+            var result = await sut.SubmitPaymentToBank(paymentRequest);
+
+            // Assert
+            Assert.AreEqual(PaymentStatuses.BankValidationError, result.PaymentStatus);
+            Assert.AreEqual(submissionDateTimeMock, result.StatusDateTime);
+        }
+
+        [Test]
+        public async Task SubmitPaymentToBank_SubmissionErrorPaymentRequest_ReturnsCorrectDetails()
+        {
+            // Arrange
+            PaymentRequest paymentRequest = new PaymentRequest
+            {
+                Amount = 100,
+                CardholderName = "Testy McTester",
+                CardNumber = "1111 2222 3333 4444",
+                CurrencyIsoAlpha3 = "GBP",
+                Cvv = "666",
+                MerchantId = new Guid("aa9f4ef9-35ab-4a10-8cf5-f5535582fd01")
+            };
+
+            var submissionDateTimeMock = new DateTimeOffset(2020, 01, 01, 0, 0, 0, new TimeSpan(0, 0, 0));
+
+            AcquiringBankResponse responseMock = new AcquiringBankResponse
+            {
+                PaymentStatus = "SubmissionError",
+                StatusDateTime = submissionDateTimeMock
+            };
+
+            var data = JsonSerializer.Serialize(responseMock);
+            var contentMock = new StringContent(data.ToString(), Encoding.UTF8, "application/json");
+
+            _httpMessageHandlerMock.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = contentMock
+                });
+
+            var client = new HttpClient(_httpMessageHandlerMock.Object);
+
+            var sut = new AcquiringBankClient(_acquiringBankSettingsMock.Object, client, _loggerMock.Object);
+
+            // Act 
+            var result = await sut.SubmitPaymentToBank(paymentRequest);
+
+            // Assert
+            Assert.AreEqual(PaymentStatuses.SubmissionError, result.PaymentStatus);
             Assert.AreEqual(submissionDateTimeMock, result.StatusDateTime);
         }
     }
